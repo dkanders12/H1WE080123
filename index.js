553d30a0504394305e4040ae5c3349104ba135a3
Webudvikler Hovedforløb <heka@techcollege.dk>
2023-11-27 10:46:55 +0100

    Song Routes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
 const port = 4000
@@ -8,17 +10,22 @@ app.get('/', (req, res) => {
 	res.send('Velkommen til min hjemmeside')
 })
 
-
-
+// Sangliste - med GET parameter eksempel
 app.get('/songs', (req, res) => {
 	console.log(req.query);
 	res.send('Sange - List alle')
 })
+
+// Sangdetaljer - med URL parameter
+app.get('/songs/:id([0-9]*)', (req, res) => {
+	console.log(req.params);
+	res.send('Sange - Detaljer')
+})
+
+// Opret ny sang
 app.post('/songs', (req, res) => {
 	res.send('Sange - Opret ny sang')
 })
-
-
 
 // 404
 app.get('*', (req, res) => {
